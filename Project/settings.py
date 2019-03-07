a96b51cc641f12b9eda0650e19194da40265c7fd
deadlyhallows <awanti2019@gmail.com>
2019-03-07 20:40:38 +0530

    added requirements.txt

--- Project/settings.py
+++ Project/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
